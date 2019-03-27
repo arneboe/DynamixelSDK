0ed592733a908603832070420ac9ccb733dddd3c
Arne Böckmann <arne.boeckmann@dfki.de>
2019-03-27 15:39:14 +0100

    even more stripped down

--- include/dynamixel_sdk/packet_handler.h
+++ include/dynamixel_sdk/packet_handler.h
@@ -21,15 +21,8 @@
 
 #ifndef DYNAMIXEL_SDK_INCLUDE_DYNAMIXEL_SDK_PACKETHANDLER_H_
 #define DYNAMIXEL_SDK_INCLUDE_DYNAMIXEL_SDK_PACKETHANDLER_H_
-
-#if defined(ARDUINO) || defined(__OPENCR__) || defined(__OPENCM904__)
-#include <Arduino.h>
-
-#define ERROR_PRINT  SerialBT2.print
-#else
 #define ERROR_PRINT  printf
 
-#endif
 
 #include <stdio.h>
 #include <vector>
